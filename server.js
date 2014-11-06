a7b4b494fc76e490d47fc3847883735f793ff10a
Kyle Chadha <kyle.chadha@buzzfeed.com>
2014-11-05 22:23:37 -0500

    Add async and begin handling of multiple pages

--- server.js
+++ server.js
@@ -3,29 +3,34 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
+var async = require("async");
+
 
 app.get('/scrape', function(req, res) {
 
   url = 'http://www.self-help-and-self-development.com/affirmations.html';
 
+  var json = {
+    categories : [],
+    affirmations : {}
+  }
+
   request(url, function(error, response, html) {
 
     if (!error) {
 
-      var $ = cheerio.load(html),
-          category,
-          affText;
-
-      var json = {
-        categories : []
-      }
+      var $ = cheerio.load(html)
 
       $('.columns_block li').filter(function() {
 
         var data = $(this);
         
         data.each(function(key, value) {
-          json.categories.push($(data[key]).find('b').text());
+
+          var category = $(data[key]).find('b').text()
+          json.categories.push(category);
+          console.log(category);
+
         });
 
       });
@@ -34,13 +39,22 @@ app.get('/scrape', function(req, res) {
       console.log(error);
     }
 
-    fs.writeFile('seeds.json', JSON.stringify(json, null, 4), function(err) {
-      console.log('File successfully written.')
-    });
-
-    res.send('Scraping Complete.');
-
   });
+  
+  // for (var i = 0; i < json.categories.length; i++) {
+
+    // $('li *:contains("' + json.categories[i] + '")')[0].click();
+    // console.log($('#ContentColumn'));
+    // console.log(json.categories[i]);
+
+  // }
+
+  // This is running FIRST... will need to use ASYNC to handle, HOLLA!
+  fs.writeFile('seeds.json', JSON.stringify(json, null, 4), function(err) {
+    console.log('File successfully written.')
+  });
+
+  res.send('Scraping Complete.');
 
 });
 
